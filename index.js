89d50af0e0d35e385d458617d446657eb3d48d0d
Benjamin Lannon <lannonbr@clarkson.edu>
2018-03-23 11:24:18 -0400

    Adding splash page for api

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const path = require("path");
+const cors = require("cors");
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200,
-    credentials: true
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+  credentials: true
 };
 
-const { schema } = require('./schema.js');
+const { schema } = require("./schema.js");
 
 const app = express();
 
 app.use(cors(corsOptions));
 
-app.use('/graphql', graphqlHTTP({
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(4000, () => {
-    console.log('Server running. Press ctrl-c to quit...');
+  console.log("Mirror GraphQL Server running. Press ctrl-c to quit...");
 });
